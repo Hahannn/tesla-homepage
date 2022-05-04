48bb04795093fee53e1ce7e68a1bf770391601e8
Han Faiz <farhanfaiznugroho@gmail.com>
2022-05-04 14:23:18 +0700

    cleanup

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <nav>
+        <h1> ReactFacts </h1>
+        <h3> React Course - Project 1</h3>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
